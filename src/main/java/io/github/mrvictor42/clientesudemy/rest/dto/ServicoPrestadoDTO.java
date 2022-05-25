da3c9ceb80dc95d8a128fb66d0d503cba773a965
MrVictor42 <guin-hu@hotmail.com>
2022-05-24 21:18:18 -0300

    Serviço prestado busca

--- src/main/java/io/github/mrvictor42/clientesudemy/rest/dto/ServicoPrestadoDTO.java
+++ src/main/java/io/github/mrvictor42/clientesudemy/rest/dto/ServicoPrestadoDTO.java
@@ -3,11 +3,18 @@ package io.github.mrvictor42.clientesudemy.rest.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Data
 @NoArgsConstructor
 public class ServicoPrestadoDTO {
+    @NotEmpty(message = "{descricao.obrigatoria}")
     private String descricao;
+    @NotEmpty(message = "{valor.obrigatoria}")
     private String valor;
+    @NotEmpty(message = "{data.obrigatoria}")
     private String data;
+    @NotNull(message = "{cliente.obrigatorio}")
     private Integer idCliente;
 }
